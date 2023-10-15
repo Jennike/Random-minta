46b9a4170e6059921843cd85e07a21f0b788afbc
Jenni Adél Szmolyán <szmolyan.jenni@edu.bme.hu>
2023-10-15 19:06:01 +0200

    szeretem a nokedlit

---
+++ Ez egy minta cukorke/Ez egy minta cukorke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ez_egy_minta_cukorke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
